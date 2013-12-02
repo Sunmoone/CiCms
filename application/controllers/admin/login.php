1fe2a7de5cc9ce0afcc04ffe310d3d5d055ad7fb
leeyong <leeyongit@163.com>
2013-12-03 00:08:16 +0800

    cicms

--- application/controllers/admin/login.php
+++ application/controllers/admin/login.php
@@ -2,39 +2,39 @@
 
 class Login extends CI_Controller {
 
-     public function __construct()
-     {
-          parent::__construct();
-
-          $this->load->helper('form');
-     }
-
-	/**
-      * 默认执行函数
-      * 
-      * @access public
-      * @return void
-      */
-	public function index() 
-  {
-    if ($this->session->userdata('admin_user'))
+    public function __construct()
     {
-      redirect('admin/dashboard', 'refresh');
-    }
-          
-		$this->load->view('admin/login_view');
-	}
+        parent::__construct();
 
-	/**
-  * 用户登出
-  * 
-  * @access public
-  * @return void
-  */
-	public function logout() 
-     {
-          $this->session->unset_userdata('admin_user');
-          $this->session->sess_destroy();
-          redirect('admin/login', 'refresh');
-	}
+        $this->load->helper('form');
+    }
+
+    /**
+    * 默认执行函数
+    * 
+    * @access public
+    * @return void
+    */
+    public function index() 
+    {
+        if ($this->session->userdata('admin_user'))
+        {
+            redirect('admin/dashboard', 'refresh');
+        }
+            
+        $this->load->view('admin/login_view');
+    }
+
+    /**
+    * 用户登出
+    * 
+    * @access public
+    * @return void
+    */
+    public function logout() 
+    {
+        $this->session->unset_userdata('admin_user');
+        $this->session->sess_destroy();
+        redirect('admin/login', 'refresh');
+    }
 }
